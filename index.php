00d3d561c2ec423c0139542fe9742431795e7e24
munnisg <gene.munnis@tufts.edu>
2026-04-26 19:04:05 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+    <?php
+        echo '<script type="text/javascript">window.location.href="Login";</script>';
+        exit;
+    ?>
+</body>
+</html>
